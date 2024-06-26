5e0d25e0b3d79223fff2373d0d3411c5079ace40
anpeher <anpeher12@gmail.com>
2024-06-27 00:07:26 +0200

    solucion error mainactivity

--- app/src/main/java/com/example/actividad1/MainActivity.kt
+++ app/src/main/java/com/example/actividad1/MainActivity.kt
@@ -4,8 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.actividad1.R
-import com.example.recyclerview.adapter.DibujosAdapter
+import com.example.actividad1.adapter.DibujosAdapter
 
 class MainActivity : AppCompatActivity() {
 
