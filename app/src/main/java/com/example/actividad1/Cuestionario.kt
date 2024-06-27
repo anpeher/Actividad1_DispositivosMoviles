6f93ff258f5963289027db45dbf5577cd95353ee
anpeher <anpeher12@gmail.com>
2024-06-27 03:31:10 +0200

    interración añadida

--- app/src/main/java/com/example/actividad1/Cuestionario.kt
+++ app/src/main/java/com/example/actividad1/Cuestionario.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.textfield.TextInputEditText
@@ -11,24 +12,46 @@ import com.google.android.material.textfield.TextInputEditText
 
 class Cuestionario : AppCompatActivity() {
 
-    lateinit var eName: TextInputEditText
-    lateinit var buttonPublicar : Button
-    lateinit var tvGoPrincipal: TextView
+    private lateinit var eName: TextInputEditText
+    private lateinit var eSerie: TextInputEditText
+    private lateinit var eUrl: TextInputEditText
+    private lateinit var buttonInsertar : Button
+    private lateinit var tvGoPrincipal: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.cuestionario)
-        tvGoPrincipal = findViewById<TextView>(R.id.tvPrincipal)
+        tvGoPrincipal = findViewById(R.id.tvPrincipal)
         tvGoPrincipal.setOnClickListener{
-            goToLogin()
+            goToPrincipal()
         }
 
+        eName = findViewById(R.id.eName)
+        eSerie = findViewById(R.id.eSerie)
+        eUrl = findViewById(R.id.eURL)
+        buttonInsertar = findViewById(R.id.buttonComentar)
+        buttonInsertar.setOnClickListener{
+
+            val name = eName.text.toString()
+            val serie = eSerie.text.toString()
+            val imageUrl = eUrl.text.toString()
+
+            if (name.isNotEmpty() && serie.isNotEmpty() && imageUrl.isNotEmpty()) {
+
+                //DibujosProvider.addDibujo(Dibujo(name, serie, imageUrl))
+                Toast.makeText(this, "Dibujo añadido!", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Debe rellenar todos los campos", Toast.LENGTH_SHORT).show()
+
+            }
+
+        }
 
     }
 
-    private fun goToLogin(){
-        val i = Intent(this,Principal::class.java)
+    private fun goToPrincipal(){
+        val i = Intent(this,MainActivity::class.java)
         startActivity(i)
     }
 }
